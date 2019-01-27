497c3c691137096817d79d861be59a1f8a6c507a
fh10178018 <1206758827@qq.com>
2019-01-27 09:53:43 +0800

    第一次上传，无venv文件

--- novel/urls.py
+++ novel/urls.py
@@ -1,6 +1,1 @@
-from django.conf.urls import url
-from novel import views
-
-urlpatterns = [
-    url(r'^$',views.index),
-]
+from django.conf.urls import urlfrom novel import viewsurlpatterns = [    url(r'^$',views.index),]
