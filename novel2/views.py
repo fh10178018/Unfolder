829cbdb1722118e2484760dfa1e1e9e1c36724c5
Emmanuel0929 <2250072993@qq.com>
2019-01-28 11:29:36 +0800

    王宪喆第一次上传尝试操作

--- novel2/views.py
+++ novel2/views.py
@@ -214,3 +214,6 @@ def webviews(x):
     else:
         demoview =Webviews.objects.get(time=data).demoview+1
         Webviews.objects.filter(time=data).update(demoview=demoview)
+
+def x():
+    print("xianzhe上传了")
