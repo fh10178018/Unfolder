f2b46dfb4fced817ba9739be73872011409be351
fh10178018 <1206758827@qq.com>
2019-03-03 17:57:40 +0800

    商家端和客户端页面完成

--- novel3/urls.py
+++ novel3/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url
 from novel3 import views
 
 urlpatterns = [
-    url(r'^$',views.home,name='home'),
-    url(r'^map$',views.map,name='map'),
+
 ]
--- novel3/views.py
+++ novel3/views.py
@@ -3,10 +3,3 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage, Invali
 from django.http import HttpResponse,HttpResponseRedirect
 
 # Create your views here.
-
-def home(request):
-    return render(request,'demo/index.html')
-
-
-def map(request):
-    return render(request,'demo/map.html')
