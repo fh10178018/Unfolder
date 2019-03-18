2cf39e240b0afb8cc50a347895f9ac096e8ae76c
fh10178018 <46178202+fh10178018@users.noreply.github.com>
2019-03-18 22:39:59 +0800

    Update urls.py

--- novel4/urls.py
+++ novel4/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path(r'<str:city>/',views.home,name='home'),
     url(r'^shop/(\d+).html$', views.shop,name='shop'),
     url(r'^s/(\w+)$', views.categories),
-    url(r'^map/(\w+)$',views.map,name='map'),
+    url(r'^map/(\w+)$',views.maps,name='map'),
     url(r'^shop/search1/$', views.search1, name='search1'),
     url(r'^shop/search2/$', views.search2, name='search2'),
     url(r'^user/login/$', views.login, name='login'),
